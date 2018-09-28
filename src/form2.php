15dfb54599f4146f80a54e7c40c3ec473cb67fbb
Andre Santos <andresantosbr@gmail.com>
2018-09-28 15:02:24 -0300

    First commit

---
+++ src/form2.php
@@ -0,0 +1,14 @@
+<?php
+    $out_nome = $_REQUEST["nome"];
+    $out_email = $_REQUEST["email"];
+    $out_cidade = $_REQUEST["cidade"];
+    
+    /*$out_fone
+    $out_cnpj
+    $out_cpf
+    $out_dtnasc
+    $out_atividade
+    */
+
+    var_dump($_REQUEST);
+?>
