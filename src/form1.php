0180160cb048385831ed40a1f0d8af83297404f9
Andre Santos <andresantosbr@gmail.com>
2018-10-01 11:03:37 -0300

    Remoção de arquivos desnecessários

---
+++ src/form1.php
@@ -0,0 +1,22 @@
+<html>
+    <body>
+        <form id="form1" name="form1" method="post" action="form2.php">
+            <p>
+            <label for="nome">Nome:<br />
+            </label>
+            <input type="text" size="70" name="nome" id="nome" required="required" />
+            </p>
+            <p>
+            <label for="email">E-mail:<br />
+            </label>
+            <input type="text" size="70" name="email" id="email" required="required" />
+            </p>
+            <p>
+            <label for="cidade">Cidade:<br />
+            </label>
+            <input type="text" size="70" name="cidade" id="cidade"  required="required"/>
+            </p>
+            <input name="cadastro" type="submit" value="Cadastre-se" />
+        </form>
+    </body>
+</html>
